4c591e48721ec521b1e3fa66938bf59f0d73a9fb
Bhavithamuthineni <163806335+Bhavithamuthineni@users.noreply.github.com>
2024-04-29 21:02:35 -0500

    Delete src directory

--- src/models/BoredModel.js
+++
@@ -1,5 +0,0 @@
-export const getBoredActivity = async () => {
-    const response = await fetch('https://www.boredapi.com/api/activity');
-    const data = await response.json();
-    return data;
-  };
